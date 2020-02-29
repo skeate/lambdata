20c7d5c5f12a67b9a1acde73e9e5f5337f233bce
Jonathan Skeate <skeate@gmail.com>
2020-02-29 15:47:40 -0500

    improvement(list): switch to Cons, add tests
    Cons is more standard. Test coverage now 100% for List.

--- src/List.ts
+++ src/List.ts
@@ -15,79 +15,76 @@ declare module 'fp-ts/lib/HKT' {
 
 export type List<A> =
   | {
-      readonly type: 'nil'
+      readonly type: 'Nil'
       readonly length: 0
     }
   | {
-      readonly type: 'node'
+      readonly type: 'Cons'
       readonly length: number
       readonly value: A
       readonly next: List<A>
     }
 
-export const nil = { type: 'nil', length: 0 } as const
+export const nil = { type: 'Nil', length: 0 } as const
 
-export const node = <A>(value: A, next: List<A> = nil): List<A> => ({
-  type: 'node',
+export const cons = <A>(value: A, next: List<A> = nil): List<A> => ({
+  type: 'Cons',
   length: next.length + 1,
   value,
   next,
 })
 
 export const arr2list = <A>(arr: Array<A>): List<A> =>
-  arr.reduceRight((p: List<A>, n) => node(n, p), nil)
+  arr.reduceRight((p: List<A>, n) => cons(n, p), nil)
 
 export const iterate = function*<A>(l: List<A>) {
-  while (l.type === 'node') {
-    yield l.value;
-    l = l.next;
+  while (l.type === 'Cons') {
+    yield l.value
+    l = l.next
   }
-  return;
+  return
 }
 
 export const list2arr = <A>(l: List<A>): Array<A> => [...iterate(l)]
 
 export const concat = <A>(xs: List<A>, ys: List<A>): List<A> =>
-  xs.type === 'nil'
+  xs.type === 'Nil'
     ? ys
-    : ys.type === 'nil'
+    : ys.type === 'Nil'
     ? xs
-    : node(xs.value, concat(xs.next, ys))
+    : cons(xs.value, concat(xs.next, ys))
 
 export const update = <A>(xs: List<A>, i: number, y: A): Option<List<A>> =>
-  xs.type === 'nil'
+  xs.type === 'Nil' || i < 0
     ? none
     : i === 0
-    ? some(node(y, xs))
-    : option.map(update(xs, i - 1, y), (n) => node(xs.value, n))
+    ? some(cons(y, xs.next))
+    : option.map(update(xs.next, i - 1, y), (n) => cons(xs.value, n))
 
 export const insert = <A>(xs: List<A>, i: number, y: A): List<A> =>
-  xs.type === 'nil'
-    ? node(y, xs)
+  xs.type === 'Nil' || i < 0
+    ? cons(y, xs)
     : i === 0
-    ? node(y, xs)
-    : node(xs.value, insert(xs.next, i - 1, y))
-
-export const suffixes = <A>(xs: List<A>): List<List<A>> =>
-  node(xs, xs.type === 'nil' ? xs : suffixes(xs.next))
+    ? cons(y, xs)
+    : cons(xs.value, insert(xs.next, i - 1, y))
 
 export const list: Functor1<URI> & Monad1<URI> = {
   URI,
 
   /* functor */
   map: (fa, f) =>
-    fa.type === 'nil' ? fa : node(f(fa.value), list.map(fa.next, f)),
+    fa.type === 'Nil' ? fa : cons(f(fa.value), list.map(fa.next, f)),
 
   /* apply */
   ap: (fab, fa) =>
-    fab.type === 'nil' || fa.type === 'nil'
+    fab.type === 'Nil' || fa.type === 'Nil'
       ? nil
       : concat(list.map(fa, fab.value), list.ap(fab.next, fa)),
   /* applicative */
-  of: (a) => node(a),
+  of: (a) => cons(a),
 
   chain: (fa, f) =>
-    fa.type === 'nil' ? fa : concat(f(fa.value), list.chain(fa.next, f)),
+    fa.type === 'Nil' ? fa : concat(f(fa.value), list.chain(fa.next, f)),
 }
 
 export const getMonoid = <A>(): Monoid<List<A>> => ({
@@ -97,8 +94,8 @@ export const getMonoid = <A>(): Monoid<List<A>> => ({
 
 export const getEq = <A>(eqa: Eq<A>): Eq<List<A>> => {
   const S: Eq<List<A>> = fromEquals((x, y) => {
-    if (x.type === 'nil' && y.type === 'nil') return true
-    if (x.type === 'node' && y.type === 'node') {
+    if (x.type === 'Nil' && y.type === 'Nil') return true
+    if (x.type === 'Cons' && y.type === 'Cons') {
       return eqa.equals(x.value, y.value) && S.equals(x.next, y.next)
     }
     return false
--- test/List.test.ts
+++ test/List.test.ts
@@ -1,4 +1,5 @@
 import * as fc from 'fast-check'
+import { eqNumber } from 'fp-ts/lib/Eq'
 import { none, some } from 'fp-ts/lib/Option'
 import * as laws from 'fp-ts-laws'
 import * as L from '../src/List'
@@ -10,7 +11,7 @@ function assert(condition: boolean): asserts condition {
 const lift = <A>(arb: fc.Arbitrary<A>): fc.Arbitrary<L.List<A>> =>
   fc.oneof(
     arb.map(() => L.nil),
-    arb.map((v) => L.node(v)),
+    arb.map((v) => L.cons(v)),
   )
 
 describe('List', () => {
@@ -18,22 +19,24 @@ describe('List', () => {
     laws.functor(L.list)(lift, L.getEq)
     laws.applicative(L.list)(lift, L.getEq)
     laws.monad(L.list)(L.getEq)
+    laws.eq(L.getEq(eqNumber), lift(fc.integer()))
+    laws.monoid(L.getMonoid<number>(), L.getEq(eqNumber), lift(fc.integer()))
   })
 
   it('provides a length', () => {
-    const list = L.node(1, L.node(2, L.nil))
+    const list = L.cons(1, L.cons(2, L.nil))
     expect(list.length).toBe(2)
   })
 
   describe('conversion helpers', () => {
     it('converts to/from array', () => {
-      const node = jest.spyOn(L, 'node')
+      const cons = jest.spyOn(L, 'cons')
       const arr = [1, 2, 3]
       const l = L.arr2list(arr)
-      expect(node).toHaveBeenCalledTimes(3)
+      expect(cons).toHaveBeenCalledTimes(3)
       expect(l.length).toBe(3)
-      node.mockRestore()
-      expect(l).toMatchObject(L.node(1, L.node(2, L.node(3, L.nil))))
+      cons.mockRestore()
+      expect(l).toMatchObject(L.cons(1, L.cons(2, L.cons(3, L.nil))))
       expect(L.list2arr(l)).toEqual(arr)
     })
   })
@@ -44,15 +47,15 @@ describe('List', () => {
     })
 
     it('concats a list with empty and get out the same list', () => {
-      const list = L.node(1, L.node(2, L.nil))
+      const list = L.cons(1, L.cons(2, L.nil))
 
       expect(L.concat(list, L.nil)).toBe(list)
       expect(L.concat(L.nil, list)).toBe(list)
     })
 
     it('concats two lists', () => {
-      const a = L.node(1, L.node(2, L.nil))
-      const b = L.node(3, L.node(5, L.nil))
+      const a = L.cons(1, L.cons(2, L.nil))
+      const b = L.cons(3, L.cons(5, L.nil))
 
       const ab = L.concat(a, b)
       expect(ab).toMatchObject({
@@ -62,8 +65,8 @@ describe('List', () => {
           next: { value: 3, next: { value: 5, next: L.nil } },
         },
       })
-      assert(ab.type === 'node')
-      assert(ab.next.type === 'node')
+      assert(ab.type === 'Cons')
+      assert(ab.next.type === 'Cons')
       expect(ab.next.next).toBe(b)
 
       const ba = L.concat(b, a)
@@ -74,8 +77,8 @@ describe('List', () => {
           next: { value: 1, next: { value: 2, next: L.nil } },
         },
       })
-      assert(ba.type === 'node')
-      assert(ba.next.type === 'node')
+      assert(ba.type === 'Cons')
+      assert(ba.next.type === 'Cons')
       expect(ba.next.next).toBe(a)
 
       expect(ab.next.next).toBe(b)
@@ -87,42 +90,49 @@ describe('List', () => {
       expect(L.update(L.nil, 24, 'bah')).toBe(none)
     })
 
-    it('updates a list', () => {
-      const list = L.node(1, L.node(2, L.nil))
-
-      // expect(L.update(list, 0, 4)).toM(list)
-      expect(L.concat(L.nil, list)).toBe(list)
+    it('updates a list with minimal replacement', () => {
+      const list = L.cons(1, L.cons(2, L.nil))
+      expect(L.update(list, 0, 4)).toEqual(some(L.arr2list([4, 2])))
+      expect(L.update(list, 1, 4)).toEqual(some(L.arr2list([1, 4])))
+      expect(L.update(list, 0, 4)).toEqual(some(L.arr2list([4, 2])))
     })
 
-    it('should concatenate two lists', () => {
-      const a = L.node(1, L.node(2, L.nil))
-      const b = L.node(3, L.node(5, L.nil))
+    it('handles out-of-bounds indices', () => {
+      const list = L.cons(1, L.cons(2, L.nil))
+      expect(L.update(list, 2, 4)).toEqual(none)
+      expect(L.update(list, -1, 4)).toEqual(none)
+    })
+  })
 
-      const ab = L.concat(a, b)
-      expect(ab).toMatchObject({
-        value: 1,
-        next: {
-          value: 2,
-          next: { value: 3, next: { value: 5, next: L.nil } },
-        },
-      })
-      assert(ab.type === 'node')
-      assert(ab.next.type === 'node')
-      expect(ab.next.next).toBe(b)
+  describe('insert', () => {
+    it('inserts elements', () => {
+      expect(L.insert(L.nil, 0, 1)).toEqual(L.cons(1))
+      const l = L.arr2list([1, 2, 6, 3])
+      expect(L.insert(l, 0, 0)).toEqual(L.arr2list([0, 1, 2, 6, 3]))
+      expect(L.insert(l, 1, 0)).toEqual(L.arr2list([1, 0, 2, 6, 3]))
+      expect(L.insert(l, 2, 0)).toEqual(L.arr2list([1, 2, 0, 6, 3]))
+      expect(L.insert(l, 3, 0)).toEqual(L.arr2list([1, 2, 6, 0, 3]))
+      expect(L.insert(l, 4, 0)).toEqual(L.arr2list([1, 2, 6, 3, 0]))
+    })
 
-      const ba = L.concat(b, a)
-      expect(ba).toMatchObject({
-        value: 3,
-        next: {
-          value: 5,
-          next: { value: 1, next: { value: 2, next: L.nil } },
-        },
-      })
-      assert(ba.type === 'node')
-      assert(ba.next.type === 'node')
-      expect(ba.next.next).toBe(a)
+    it('handles out-of-bounds', () => {
+      const l = L.arr2list([1, 2, 6, 3])
+      expect(L.insert(l, -1, 0)).toEqual(L.arr2list([0, 1, 2, 6, 3]))
+      expect(L.insert(l, 5, 0)).toEqual(L.arr2list([1, 2, 6, 3, 0]))
+    })
+  })
 
-      expect(ab.next.next).toBe(b)
+  describe('eq', () => {
+    it('checks equality of two lists', () => {
+      expect(L.getEq(eqNumber).equals({ type: 'Nil', length: 0 }, L.nil)).toBe(
+        true,
+      )
+      const l1 = L.arr2list([1, 2, 3])
+      const l2 = L.arr2list([1, 2, 3])
+      const l3 = L.arr2list([4, 1, 2, 3])
+      expect(L.getEq(eqNumber).equals(l1, l2)).toBe(true)
+      expect(L.getEq(eqNumber).equals(l1, l3)).toBe(false)
+      expect(L.getEq(eqNumber).equals(L.insert(l2, 0, 4), l3)).toBe(true)
     })
   })
 })
