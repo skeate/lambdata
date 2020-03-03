483fd2c0e312fb26230124a80e9250a913afb23f
Jonathan Skeate <skeate@gmail.com>
2020-03-03 09:33:59 -0500

    chore: upgrade deps, adjust packaging

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export * as List from './List'
+
+export * as PSet from './PSet'
+export * as BinaryTree from './BinaryTree'
+
+export * as PHeap from './PHeap'
+export * as LeftistHeap from './LeftistHeap';
+export * as BinomialHeap from './BinomialHeap';
